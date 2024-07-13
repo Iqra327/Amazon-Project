96baae7607b03acd21028f7be28b4e9737925f32
Iqra327 <iqrarasheed219@gmail.com>
2024-07-13 22:30:20 +0500

    initial-commit

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import {cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { deliveryOptions } from '../data/deliveryOptions.js';
 
 const today = dayjs();
 const deliveryDate = today.add(7, 'days');
@@ -98,6 +100,12 @@ cart.forEach((cartItem)=>{
     </div>
   `
 });
+
+function deliveryOptionsHTML(){
+  deliveryOptions.forEach(() => {
+    
+  })
+}
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
